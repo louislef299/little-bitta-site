94fcd27a72c24577b1643cc281e0cde1caa643b8
Louis LeFebvre <louislefebvre1999@gmail.com>
2025-12-31 14:42:53 -0600

    fix: Responsive cart functionality

--- src/routes/cart/+page.svelte
+++ src/routes/cart/+page.svelte
@@ -1,7 +1,16 @@
 <script>
-    import { cart, clearCart } from '$lib/cart.svelte';
+    import { cart, clearCart, getCartTotal } from '$lib/cart.svelte';
+
+    let total = $derived(getCartTotal());
 </script>
 
 <div>Cart has {cart.items.length} items</div>
+
+<ul>
+    {#each cart.items as item}
+        <li>{item.name} x {item.quantity}</li>
+    {/each}
+</ul>
+<h3>Total is ${total}</h3>
 
 <button type="button" onclick={clearCart}>Clear Cart</button>
--- src/routes/shop/+page.svelte
+++ src/routes/shop/+page.svelte
@@ -2,12 +2,10 @@
     import { addToCart } from '$lib/cart.svelte';
 
     var items = [
-        { id: "1", name: "one", price: 3, img: "/images/granola-generic.jpg"},
-        { id: "2", name: "two", price: 3, img: "/images/granola-generic.jpg"},
-        { id: "3", name: "three", price: 3, img: "/images/granola-generic.jpg"},
-        { id: "4", name: "four", price: 3, img: "/images/granola-generic.jpg"},
-        { id: "5", name: "five", price: 3, img: "/images/granola-generic.jpg"},
-        { id: "6", name: "six", price: 3, img: "/images/granola-generic.jpg"}
+        { id: "1", name: "Peanut Butter Nutella", price: 10, img: "/images/granola-generic.jpg"},
+        { id: "2", name: "Pistachio", price: 10, img: "/images/granola-generic.jpg"},
+        { id: "3", name: "Peanut Butter Chocolate Chip", price: 10, img: "/images/granola-generic.jpg"},
+        { id: "4", name: "Honey Bear", price: 10, img: "/images/granola-generic.jpg"}
     ]
 </script>
 
@@ -32,8 +30,9 @@
 		<li>
             <img class="shop" alt="{item.name} image" src={item.img} />
             <br />
-            {item.name} x {item.price}
-            <button type="button" onclick={() => addToCart(item)}>Add 2 Cart</button>
+            {item.name} @ ${item.price}/lb
+            <br />
+            <button type="button" onclick={() => addToCart(item)}>+Cart</button>
         </li>
 	{/each}
 </ul>
