26a49645fdf837b937bcc499417ac6b8c282333e
Louis LeFebvre <louislefebvre1999@gmail.com>
2025-12-30 22:30:09 -0600

    feat: Super basic granola shop page done

--- src/routes/shop/+page.svelte
+++ src/routes/shop/+page.svelte
@@ -1,1 +1,36 @@
-<h1>shop</h1>
+<script>
+    var items = [
+        { name: "one", qty: 3, img: "/images/granola-generic.jpg"},
+        { name: "two", qty: 3, img: "/images/granola-generic.jpg"},
+        { name: "three", qty: 3, img: "/images/granola-generic.jpg"},
+        { name: "four", qty: 3, img: "/images/granola-generic.jpg"},
+        { name: "five", qty: 3, img: "/images/granola-generic.jpg"},
+        { name: "six", qty: 3, img: "/images/granola-generic.jpg"}
+    ]
+</script>
+
+<style>
+    .shop {
+        width: 10rem;
+        height: 14rem;
+    }
+
+    ul {
+      list-style: none;
+      padding: 0;
+      display: grid;
+      grid-template-columns: repeat(auto-fit, minmax(10rem, 1fr));
+      gap: 1rem;
+    }
+</style>
+
+<h1>Granola Available</h1>
+<ul>
+	{#each items as item}
+		<li>
+            <img class="shop" alt="{item.name} image" src={item.img} />
+            <br />
+            {item.name} x {item.qty}
+        </li>
+	{/each}
+</ul>
