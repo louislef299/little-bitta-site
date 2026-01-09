8637b42d106d9799e462c89eeebfbf91bebfc758
Louis LeFebvre <louislefebvre1999@gmail.com>
2026-01-09 16:12:42 -0600

    docs(comment): Add next steps for how to manage drop limits

--- src/lib/cart/drops.svelte.ts
+++ src/lib/cart/drops.svelte.ts
@@ -3,6 +3,21 @@ export type Drop = {
     long: string,
 }
 
+/***
+ * For next time:
+ * 
+ * Update the drop-based system to just invalidate the +Cart button when a drop
+ * limit has been reached. Then, make a +Waitlist button appear to add items to
+ * a waitlist. With the waitlist functionality, will need to make user profiles
+ * possible so that users can check-in on their order status.
+ * 
+ * Do we want that? Is that too much complexity for now? Maybe instead, I'll
+ * just grey out the +Cart button and have an interactive graph that makes it
+ * clear that we are at capacity for the current drop date and tell users when
+ * they can return again for more granola.
+ * 
+***/
+
 export function getDrops(): Drop[] {
     return [
         {
