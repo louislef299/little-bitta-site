965218b09310a6e0aab7c00f2381c681182300e1
Louis LeFebvre <louislefebvre1999@gmail.com>
2026-01-19 12:14:50 -0600

    chore: Remove paypal types

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,7 +1,5 @@
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
-import { PayPalNamespace } from "@paypal/paypal-js";
-
 declare global {
   namespace App {
     // interface Error {}
@@ -9,11 +7,6 @@ declare global {
     // interface PageData {}
     // interface PageState {}
     // interface Platform {}
-  }
-
-  // Window augmentation for PayPal SDK
-  interface Window {
-    paypal?: PayPalNamespace;
   }
 }
 
