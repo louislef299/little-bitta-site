98aa5a95f07f4cc7b4fdef449fa331c05a060941
Louis LeFebvre <louislefebvre1999@gmail.com>
2026-01-24 14:10:55 -0600

    style: Add Hammer & Anvil footer link & email

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,14 +1,43 @@
+<script lang="ts">
+    import { Mail } from '@lucide/svelte';
+</script>
+
 <footer>
-    © 2026 Louis LeFebvre.
+    <div class="footer-left">
+        © 2026 <a href="https://hammer-anvil.com">Hammer & Anvil.</a>
+    </div>
+    <div class="footer-right">
+        <a href="mailto:llefebvre@hammer-anvil.com">
+            <Mail />
+        </a>
+    </div>
 </footer>
 
 <style>
     footer {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
         margin-top: 3rem;
         padding-top: 1.5rem;
         padding-bottom: 0.5rem;
         color: #888;
         font-size: 0.9rem;
+    }
+
+    footer a {
+        color: #888;
+    }
+
+    footer a:hover {
+        color: var(--accent-color);
+    }
+
+    .footer-left {
         text-align: left;
+    }
+
+    .footer-right {
+        text-align: right;
     }
 </style>
