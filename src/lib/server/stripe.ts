b081a091bcb8b8beb49726ce161bdbd6280cc777
Louis LeFebvre <louislefebvre1999@gmail.com>
2026-02-02 13:29:25 -0600

    fix(stripe): Remove the apiVersion configuration
    this just uses the latest available version now

--- src/lib/server/stripe.ts
+++ src/lib/server/stripe.ts
@@ -8,8 +8,11 @@ export function getStripe(): Stripe {
     if (!env.SECRET_STRIPE_KEY) {
       throw new Error("STRIPE_SECRET_KEY is required");
     }
+
+    // https://www.npmjs.com/package/stripe#configuration
     _stripe = new Stripe(env.SECRET_STRIPE_KEY, {
-      apiVersion: "2025-12-15.clover",
+      maxNetworkRetries: 2,
+      protocol: 'https',
     });
   }
   return _stripe;
