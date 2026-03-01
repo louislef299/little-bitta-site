00ed8da1cd6f83e25db4ed8d5c63430d10988e37
Louis LeFebvre <louislefebvre1999@gmail.com>
2026-03-01 13:12:00 -0600

    docs: Update about page for Isabelle

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,26 +1,27 @@
 <div class="about-us">
-    <h2>About Us</h2>
-    <p>
-        Little Bitta is a small-batch, baked goods company based in Minnesota. We
-        believe everyone deserves a little treat(or several!) throughout the day.
-        Our mission is to make our community a little sweeter by providing treat
-        that taste just like home. From the time you wake up, to right before
-        bed, we have a Little Bitta something just for you.
-    </p>
+  <h2>About Isabelle</h2>
+  <p>
+    Little Bitta is a small-batch, baked goods company based in Minnesota. We
+    believe everyone deserves a little treat(or several!) throughout the day.
+    Our mission is to make our community a little sweeter by providing treat
+    that taste just like home. From the time you wake up, to right before bed,
+    we have a Little Bitta something just for you.
+  </p>
 
-    <p>
-        With everything made locally, and by the pro herself, you can comfortably
-        know that you will get the best taste and quality in your granola, every
-        time. To learn more about our story, check out <a
-        href="/about"><code>/about</code></a>.
-    </p>
+  <p>
+    With everything made locally, and by the pro herself, you can comfortably
+    know that you will get the best taste and quality in your granola, every
+    time. To learn more about our story, check out <a href="/about"
+      ><code>/about</code></a
+    >.
+  </p>
 
-    <h2>Contact Michele Directly</h2>
-    <p>
-        We are currently a small brand and only allow orders to run through
-        email. If you would like to try out our granola, feel free to email us at
-        littlebittagranola@gmail.com with your name, address and order request
-        and we will get back to you as soon as possible. We currently only
-        operate in the Minnesota metro area.
-    </p>
+  <h2>Contact Michele Directly</h2>
+  <p>
+    We are currently a small brand and only allow orders to run through email.
+    If you would like to try out our granola, feel free to email us at
+    littlebittagranola@gmail.com with your name, address and order request and
+    we will get back to you as soon as possible. We currently only operate in
+    the Minnesota metro area.
+  </p>
 </div>
